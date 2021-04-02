75edb76da01c7befc555b07dd6de37f9746b0ee6
John Westover <78867913+Jowest015@users.noreply.github.com>
2021-04-02 16:45:39 -0500

    debugging

---
+++ madlib.js
@@ -0,0 +1,8 @@
+var student = 'Marcus';
+var subject = 'Psychology';
+
+function madLib () {
+  return student + "'s favorite subject in school is " + subject
+}
+
+console.log(madLib(student, subject))
---
+++ tip-calc.js
@@ -0,0 +1,9 @@
+var tipStuff = {
+  good: .2,
+  fair: .15,
+  bad: .1,
+  bill: 55
+};
+
+
+console.log(tipStuff)
