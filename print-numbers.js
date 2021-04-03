1591c7e4ec51261ae6a1a4e6f9b68a268f4c609c
John Westover <78867913+Jowest015@users.noreply.github.com>
2021-04-03 18:31:45 -0500

    Create print-numbers.js

---
+++ print-numbers.js
@@ -0,0 +1,18 @@
+//for-loop
+function printNumbers (start) {
+  for(var i = start; i < start + 11; i++) {
+  console.log(i);
+  }
+}
+
+printNumbers(0); 
+
+//while function
+let total = 0;
+let limit = 10;
+
+while (total < limit) {
+  total += 1;
+  console.log(total);
+}
+
