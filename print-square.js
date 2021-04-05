15948b427743ec27be74dd7980530567b3207633
John Westover <78867913+Jowest015@users.noreply.github.com>
2021-04-05 15:56:42 -0500

    Update print-square.js

--- print-square.js
+++ print-square.js
@@ -1,7 +1,13 @@
 var size = 5;
 var i = 0;
 
-for (var i = 0; i < size; i++){
+function printSquare() {
+  for (var i = 0; i < size; i++){
   for (var j = 0; j < size; j++)
-      console.log("*");
-}//thought that would work...how do i get javascript to make columns?
+      console.log("*******");
+      console.log("\n");
+  }
+}
+
+console.log(printSquare(5))
+//thought that would work...how do i get javascript to make columns?
